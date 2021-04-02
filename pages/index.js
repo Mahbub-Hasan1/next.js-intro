15741ae09b5f202c985681c81f288f4aba9d9865
Mahbub Hasan <mahbub.hasan.9000@gmail.com>
2021-04-02 11:26:42 +0000

    done

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>Hello <Link href="/posts/posts"><a>world</a></Link></main>
+      <main className={styles.main}>Hello wow nice <Link href="/posts/posts"><a>world</a></Link></main>
 
       <footer className={styles.footer}>
         <a
